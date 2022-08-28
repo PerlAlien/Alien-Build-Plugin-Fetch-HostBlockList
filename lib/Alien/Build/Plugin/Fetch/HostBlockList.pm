4bde0e7b134d69f62fe2c75d629fdb398264b5a3
Graham Ollis <plicease@cpan.org>
2022-08-28 12:13:21 -0600

    Initial structure

---
+++ lib/Alien/Build/Plugin/Fetch/HostBlockList.pm
@@ -0,0 +1,12 @@
+package Alien::Build::Plugin::Fetch::HostBlockList;
+
+use strict;
+use warnings;
+use 5.008004;
+
+# ABSTRACT: Reject any Alien::Build fetch requests going to hosts in the block list
+# VERSION
+
+1;
+
+
---
+++ t/alien_build_plugin_fetch_hostblocklist.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Alien::Build::Plugin::Fetch::HostBlockList;
+
+ok 1, 'todo';
+
+done_testing;
+
+
